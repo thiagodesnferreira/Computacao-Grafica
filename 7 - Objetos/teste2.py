2a1d30ab94b8eed9a6bff6a4b36504f15e401078
Thiago Ferreira <thiagodesnferreira@gmail.com>
2019-07-03 17:04:03 -0300

    P2 - Entrega FInal
    Entrega dos exercícios referentes a P2, de iluminação, textura e sombra.
    Todos dentro do projeto final.

--- 7 - Objetos/teste2.py
+++ 7 - Objetos/teste2.py
@@ -1,13 +1,13 @@
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 from OpenGL.GL import *
-from OBJFileLoader import *
+from PYFileLoader import *
 
 def display():
-    global obj
+    global py
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
     glRotatef(2,1,3,0)
-    glCallList(obj.gl_list)
+    glCallList(py.gl_list)
     glutSwapBuffers()
  
 def timer(i):
@@ -23,7 +23,7 @@ def reshape(w,h):
     gluLookAt(0,1,10,0,0,0,0,1,0)
 
 def init():
-    global obj
+    global py
 
     glLightfv(GL_LIGHT0, GL_POSITION,  (5, 5, 5, 1.0))
     glLightfv(GL_LIGHT0, GL_AMBIENT, (0.4, 0.4, 0.4, 1.0))
@@ -35,13 +35,13 @@ def init():
     glShadeModel(GL_SMOOTH)
     glEnable(GL_DEPTH_TEST)
     glEnable(GL_MULTISAMPLE)
-    obj = OBJ("al.obj")
+    py = PY("bunny/reconstruction/bun_zipper.ply")
 
 def main():
     glutInit(sys.argv)
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_MULTISAMPLE)
     glutInitWindowSize(800,600)
-    glutCreateWindow("Obj")
+    glutCreateWindow("Py")
     glutReshapeFunc(reshape)
     glutDisplayFunc(display)
     glutTimerFunc(50,timer,1)
